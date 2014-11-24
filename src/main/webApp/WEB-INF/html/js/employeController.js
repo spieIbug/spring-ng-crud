d91589e349a1fb85c4e29b617ae2a0fe5f5eed7c
Yacine <my.meddah@gmail.com>
2014-11-24 14:28:02 +0100

    [MYM] adding refresh list call

--- src/main/webApp/WEB-INF/html/js/employeController.js
+++ src/main/webApp/WEB-INF/html/js/employeController.js
@@ -50,6 +50,7 @@
 																		+ "<button type=\"button\" class=\"close\" data-dismiss=\"alert\"><span aria-hidden=\"true\">&times;</span><span class=\"sr-only\">Close</span></button>"
 																		+ "<strong>Succ&egrave;s!</strong> Employe added!."
 																		+ "</div>");
+												refreshEmployeTable(response);
 											}).error(function(response) {
 										alert('erreur insertion');
 									});
